2f90bccfcf7872f47e23eb13de62aac01d20c227
ongks <45529850+ongks-useR@users.noreply.github.com>
2024-02-07 08:06:46 +0800

    getUserRequest --> prompt user to input square grid when click on the start button. This is a helper function for sketchPad. sketchPad will get output from getUserRequest and return numberOfBox to reflect in the outer box. Height & width of each grid box is determined by user input for square grid.

--- sketch.js
+++ sketch.js
@@ -1,14 +1,38 @@
-const root = document.querySelector('body');
-const totalBox = (16 * 16) // 16 by 16 grid of square boxes;
+function getUserRequest() {
 
-for (let i = 1; i <= totalBox; i++) {
+    let squareGrid = prompt('Please input Square Grid:');
 
-    const box = document.createElement('div');
+    // Only proceed if user provide a number OR an interger
+    if ((squareGrid != null) || (Number.isInteger(squareGrid))) {
+        squareGrid = parseInt(squareGrid)
+    }
 
-    // height and width of each box based on outer box 600px and divided among totalBox
-    const height = 600 / 16;
-    const width = 600 / 16;
-    box.style.cssText = `height: ${height}px; width: ${width}px;`
-
-    root.appendChild(box);
+    return squareGrid;
 }
+
+function sketchPad() {
+
+    // Clear the sketch pad each time user click the "Start" button
+    const root = document.querySelector('#container');
+    root.textContent = '';
+
+    let squareGrid = getUserRequest();
+    let numberOfBox = squareGrid ** 2;
+
+    // Draw the square box
+    for (let i = 1; i <= numberOfBox; i++) {
+
+        const box = document.createElement('div');
+
+        // height and width of each box based on outer box 600px and divided among numberOfBox
+        const height = 600 / squareGrid;
+        const width = 600 / squareGrid;
+
+        box.style.cssText = `height: ${height}px; width: ${width}px;`
+
+        root.appendChild(box);
+    }
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', sketchPad);
